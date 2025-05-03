c1c1a03e50da86edce8537f26ae285ccdce2042e
Erfan sheikhhoseini <erfan.sheikhhoseini@gmail.com>
2025-05-03 22:48:34 +0330

    .

---
+++ Assets/Erfan/GS.cs
@@ -0,0 +1,16 @@
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "GeneralSettings", menuName = "Razhia/GeneralSettings", order = 0)]
+public class GS : SingletonScriptableObject<GS>
+{
+    [TitleGroup("UI")]
+    public float CBButtonsAnimateTime;
+    public Ease CBButtonsOffEase;
+    public Ease CBButtonsOnEase;
+    
+    public Ease FadeOutEase;
+    public Ease FadeIntEase;
+}
