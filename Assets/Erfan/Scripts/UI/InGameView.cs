172006b75160c0ea3a4a8fede4d86070db34464e
Erfan sheikhhoseini <erfan.sheikhhoseini@gmail.com>
2025-05-22 10:11:32 +0330

    .

--- Assets/Erfan/Scripts/UI/InGameView.cs
+++ Assets/Erfan/Scripts/UI/InGameView.cs
@@ -3,6 +3,7 @@ using Joyixir.GameManager.UI;
 using RTLTMPro;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class InGameView : View
@@ -11,13 +12,28 @@ public class InGameView : View
     public RTLTextMeshPro userRights;
     public RTLTextMeshPro userWrongs;
     public Timer timer;
+    public Button restartButton;
+    public Button homeButton;
 
+
+    private void OnEnable()
+    {
+        homeButton.onClick.AddListener(() => GameManager.Instance.LoadScene(0));
+        restartButton.onClick.AddListener(() => GameManager.Instance.RestartCurrentLevel());
+    }
+    
+    private void OnDisable()
+    {
+        homeButton.onClick.RemoveAllListeners();
+        restartButton.onClick.RemoveAllListeners();
+    }
+
+    
     private void Start()
     {
         userRights.text = "0";
         userWrongs.text = "0";
     }
-
     protected override void OnBackBtn()
     {
     }
